0f71d9aef1942126b0837857e37c877861531278
Paul Julian <bpj101@dameagnesproductions.com>
2018-09-22 10:41:26 -0400

    Added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
--- src/materialize.js
+++ src/materialize.js
@@ -1,43 +1,63 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var side_instances = M.Sidenav.init(elems);
-});
+  // Sidenav
+  const side_nav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(side_nav);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var modal_instances = M.Modal.init(elems);
-});
+  // Modal
+  const modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.materialboxed');
-  var materialboxed_instances = M.Materialbox.init(elems);
-});
+  // Materialbox
+  const mBox = document.querySelectorAll('.materialboxed');
+  M.Materialbox.init(mBox);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var parallax_instances = M.Parallax.init(elems);
-});
+  // Parallax
+  const parallax = document.querySelectorAll('.parallax');
+  M.Parallax.init(parallax);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.tabs');
-  var tabs_instances = M.Tabs.init(elems);
-});
+  // Tabs
+  const tabs = document.querySelectorAll('.tabs');
+  M.Tabs.init(tabs);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var options = { disableWeekends: true };
-  var elems = document.querySelectorAll('.datepicker');
-  var datepicker_instances = M.Datepicker.init(elems, options);
-});
+  // Datepicker
+  const date_options = { disableWeekends: true };
+  const date_picker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(date_picker, date_options);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var options = { position: 'bottom' }
-  var elems = document.querySelectorAll('.tooltipped');
-  var tooltip_instances = M.Tooltip.init(elems, options);
-});
+  // Tooltip
+  const tool_options = { position: 'bottom' }
+  const tool = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(tool, tool_options);
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.scrollspy');
-  var scroll_instances = M.ScrollSpy.init(elems);
+  // Scrollspy
+  const scroll = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(scroll);
+
+  // Slider
+  const slider_options = {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+  };
+  const slider = document.querySelectorAll('.slider');
+  M.Slider.init(slider, slider_options);
+
+  // Autocomplete
+  const autoOptions = {
+    data: {
+      "Aruba": null,
+      "Cancun Mexico": null,
+      "Hawaii": null,
+      "Florida": null,
+      "California": null,
+      "Jamacia": null,
+      "Europe": null,
+    }
+  }
+  const autoComplete = document.querySelector('.autocomplete');
+  M.Autocomplete.init(autoComplete, autoOptions);
+
 });
 
 
